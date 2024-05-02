8be9304ca596349ab01b2146d4f2d7cd239793b2
bo-values <bo.takuma@valuesccg.com>
2024-05-02 10:21:08 +0000

    fix: wip

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // webpack: (config, { webpack }) => {
-    //     config.plugins.push(new webpack.IgnorePlugin({
-    //         resourceRegExp: /^pg-native$|^cloudflare:sockets$|^node:stream$/,
-    //     }))
-    //     return config
-    // },
+    webpack: (config, { webpack }) => {
+        config.plugins.push(new webpack.IgnorePlugin({
+            resourceRegExp: /^pg-native$|^cloudflare:sockets$|^node:stream$/,
+        }))
+        return config
+    },
 }
 
 export default nextConfig
