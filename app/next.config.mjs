0eb925d5ed4e9e888833e052a7d5dcc56c83c3bb
bo-values <bo.takuma@valuesccg.com>
2024-05-07 12:02:42 +0000

    wip

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // basePath: "",
-    // output: "export",
-    // images: { unoptimized: true },
-    // webpack: (config, { webpack }) => {
-    //     config.plugins.push(new webpack.IgnorePlugin({
-    //         resourceRegExp: /^pg-native$|^cloudflare:sockets$/,
-    //         // resourceRegExp: /^pg-native$|^cloudflare:sockets$|^node:stream$/,
-    //     }))
-    //     // config.plugins.push(new webpack.NormalModuleReplacementPlugin(/^node:/, (resource) => {
-    //     //     resource.request = resource.request.replace(/^node:/, '');
-    //     //   }))
-    //     return config
-    // },
-}
+const nextConfig = {}
 
 export default nextConfig
